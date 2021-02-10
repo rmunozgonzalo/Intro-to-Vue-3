9b5adbbe0e3bcb04efb0f89ed6c7541d03248cbe
rmunozgonzalo <munoz.rojas.gonzalo@gmail.com>
2021-02-09 22:44:51 -0300

    solution l6

--- main.js
+++ main.js
@@ -11,5 +11,21 @@ const app = Vue.createApp({
               { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ]
         }
+    },
+    methods: {
+        addToCart() {
+          this.cart += 1
+        },
+        removeToCart(){
+          if(this.cart > 0 ){
+            this.cart -=1
+          }
+          else {
+            this.cart = 0
+          }
+        },
+        updateImage(variantImage){
+          this.image = variantImage
+        }
     }
 })
